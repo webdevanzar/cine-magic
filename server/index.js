5d93b3fe5be30316d280ad9f46386f4572b623d3
Muhammed anzarshah <anzarsha3240@gmail.com>
2025-02-17 21:09:51 +0530

    added backend render.com link in admin-panel

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ connectDb();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5175"],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
