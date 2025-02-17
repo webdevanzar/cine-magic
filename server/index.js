df4adaccfb575333082d8794f7ca5d5d3f2af42a
Muhammed anzarshah <anzarsha3240@gmail.com>
2025-02-17 21:29:52 +0530

    add admin panel's url to cors on server

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ connectDb();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://cine-magic-gamma.vercel.app"],
     credentials: true,
   })
 );
