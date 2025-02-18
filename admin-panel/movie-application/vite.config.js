a7379e9f8d62ddec1069731a81a0dcada87c40b1
Muhammed anzarshah <anzarsha3240@gmail.com>
2025-02-18 19:45:55 +0530

    modified viteconfig.js

--- admin-panel/movie-application/vite.config.js
+++ admin-panel/movie-application/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   base: "/",
   server: {
     historyApiFallback: true, // Ensures Vite serves index.html for all routes
